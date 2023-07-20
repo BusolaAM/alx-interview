6a4356621dce654a4465b564c94ed05425b76985
BusolaAM <busolaatinsola@gmail.com>
2023-07-20 08:22:12 +0100

    0x03. Log Parsing - taskss update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,41 +1,68 @@
 #!/usr/bin/python3
 """Write a script that reads stdin line by line and computes metrics:
 
+Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+<status code> <file size> (if the format is not this one, the line
+must be skipped)
+After every 10 lines and/or a keyboard interruption (CTRL + C),
+print these statistics from the beginning:
+Total file size: File size: <total size>
+where <total size> is the sum of all previous <file size>
+(see input format above)
+Number of lines by status code:
+possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
+if a status code doesn’t appear or is not an integer,
+don’t print anything for this status code
+format: <status code>: <number>
+status codes should be printed in ascending order
+
+line list = [<IP Address>, -, [<date>], "GET /projects/260 HTTP/1.1",
+<status code>, <file size>]
 """
 
+
 import sys
-from collections import defaultdict
 
-def parse_line(line):
-    try:
-        _, _, _, _, _, status_code, file_size = line.split()[0:7]
-        return int(status_code), int(file_size)
-    except (ValueError, IndexError):
-        return None, None
+# store the count of all status codes in a dictionary
+status_codes_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+                     '404': 0, '405': 0, '500': 0}
 
-def print_statistics(total_file_size, status_counts):
-    print(f"File size: {total_file_size}")
-    for status_code in sorted(status_counts.keys()):
-        print(f"{status_code}: {status_counts[status_code]}")
+total_size = 0
+count = 0  # keep count of the number lines counted
 
-def main():
-    total_file_size = 0
-    status_counts = defaultdict(int)
-    line_count = 0
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
 
-    try:
-        for line in sys.stdin:
-            status_code, file_size = parse_line(line.strip())
-            if status_code is not None and file_size is not None:
-                total_file_size += file_size
-                status_counts[status_code] += 1
-                line_count += 1
+        if len(line_list) > 4:
+            status_code = line_list[-2]
+            file_size = int(line_list[-1])
 
-                if line_count % 10 == 0:
-                    print_statistics(total_file_size, status_counts)
-    except KeyboardInterrupt:
-        print("\nKeyboard interruption received. Printing current statistics.")
-        print_statistics(total_file_size, status_counts)
+            # check if the status code receive exists in the dictionary and
+            # increment its count
+            if status_code in status_codes_dict.keys():
+                status_codes_dict[status_code] += 1
 
-if __name__ == "__main__":
-    main()
+            # update total size
+            total_size += file_size
+
+            # update count of lines
+            count += 1
+
+        if count == 10:
+            count = 0  # reset count
+            print('File size: {}'.format(total_size))
+
+            # print out status code counts
+            for key, value in sorted(status_codes_dict.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
+    pass
+
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(status_codes_dict.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
